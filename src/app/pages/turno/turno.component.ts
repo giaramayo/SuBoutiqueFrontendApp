b583b08c0553a77d5b0f40e25e815db6798308d7
giaramayo <84822366+giaramayo@users.noreply.github.com>
2022-05-05 16:18:03 -0300

    turnos

--- src/app/pages/turno/turno.component.ts
+++ src/app/pages/turno/turno.component.ts
@@ -10,12 +10,43 @@ export class TurnoComponent {
 
   public selected?: Date | null;
   public hoy: Date;
+  public turnos: any[];
 
   public abrirPanel: boolean;
 
   constructor() { 
     this.hoy = new Date();
     this.abrirPanel = false;
+
+    this.turnos = [{
+      id: 1,
+      paciente: {
+        dni: 1111,
+        nombre: "Lucia",
+        apellido: "H"
+      },
+      tratamiento: {
+        nombre: 'Limpieza'
+      },
+      estado: 'CONFIRMADO',
+      fechaHora: new Date(),
+      observacion: 'ddd'
+    },
+    {
+      id: 1,
+      paciente: {
+        dni: 3333,
+        nombre: "Sofia",
+        apellido: "K"
+      },
+      tratamiento: {
+        nombre: 'Masajes'
+      },
+      estado: 'CANCELADO',
+      fechaHora: new Date(),
+      observacion: 'ddd'
+    }];
+
   }
 
 
