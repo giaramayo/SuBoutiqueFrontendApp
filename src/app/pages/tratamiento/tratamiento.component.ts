45371d409914069b6d73f50ce923d0db8d0b3bef
giaramayo <84822366+giaramayo@users.noreply.github.com>
2022-06-02 12:41:51 -0300

    filtro tratamiento

--- src/app/pages/tratamiento/tratamiento.component.ts
+++ src/app/pages/tratamiento/tratamiento.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { TratamientoService } from 'src/app/service/tratamiento.service';
 import { DialogModificarTratamientoComponent } from '../../component/dialog-modificar-tratamiento/dialog-modificar-tratamiento.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DialogSnackbarComponent } from '../../component/dialog-snackbar/dialog-snackbar.component';
 import { ConfirmacionComponent } from '../../component/confirmacion/confirmacion.component';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-tratamiento',
@@ -15,18 +15,14 @@ import { ConfirmacionComponent } from '../../component/confirmacion/confirmacion
 export class TratamientoComponent implements OnInit {
 
   public dataSource: any;
-  public tratamientoForm: FormGroup;
   public botonFiltroLoading: boolean;
   public displayedColumns: string[] = ['descripcion', 'duracion', 'precio', 'modif', 'elim' ];
-
 
   constructor(public _snackBar: MatSnackBar,
               public dialog: MatDialog,
               private tratamientoService: TratamientoService) { 
     this.botonFiltroLoading = false;
-    this.tratamientoForm = new FormGroup({
-      descripcion:  new FormControl("")
-   });
+    
   }
 
   ngOnInit(): void {
@@ -35,7 +31,7 @@ export class TratamientoComponent implements OnInit {
 
   getTratamientos(){
     this.tratamientoService.getAll().subscribe( resp => {
-      this.dataSource = resp;
+      this.dataSource = new MatTableDataSource(resp);
     },
     () => {
       this._snackBar.openFromComponent(DialogSnackbarComponent,{ 
@@ -48,14 +44,7 @@ export class TratamientoComponent implements OnInit {
     });
   }
 
-  buscar() {
-    this.tratamientoService.getFiltrar(this.tratamientoForm.get('descripcion')?.value)
-    .subscribe( resp => {
-      this.dataSource = resp;
-    });
-  }
-
-  agregar(){
+  agregar() {
     const nuevo = {
       descripcion: '',
       duracion: 0,
@@ -82,18 +71,6 @@ export class TratamientoComponent implements OnInit {
     });
   }
 
-  validar(): boolean {
-    return!this.tratamientoForm.get('descripcion')?.value;
-  }
-
-
-
-
-
-
-
-
-
   modificar( element: any){
     element.titulo = 'Modificar Tratamiento';
     const dialogRef = this.dialog.open(DialogModificarTratamientoComponent, {
@@ -115,23 +92,8 @@ export class TratamientoComponent implements OnInit {
     });
   }
 
-  // getTratamientos(){
-  //   this.tratamientoService.getAll().subscribe( resp => {
-  //     this.dataSource = resp;
-  //   }, () => {
-  //     this._snackBar.openFromComponent(DialogSnackbarComponent,{ 
-  //       data: { icono: 'report', mensaje: "Ocurrio un error al consultar los tratamientos", titulo: 'Error'},
-  //       duration: 4000,
-  //       horizontalPosition: "right",
-  //       verticalPosition: "top",
-  //       panelClass: ["snack-bar-err"]
-  //     });
-  //   });
-  // }
-
   eliminar( element: any){
     const dialogRef = this.dialog.open(ConfirmacionComponent, {
-      //width: '270px',
       data: {
           msj: "¿Esta seguro que desea eliminar \"" + element.descripcion + "\"?",
           titulo: "Eliminar"
@@ -168,6 +130,13 @@ export class TratamientoComponent implements OnInit {
         () => {
           this.getTratamientos();
         });
+  }
+
+  filtrar(event: Event) {
+    
+    const filterValue = (event.target as HTMLInputElement).value;
+    console.log(filterValue)
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
