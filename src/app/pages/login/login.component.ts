ad0d0a06c5224e6aeb71449d23f5e6275406c4e9
giaramayo <84822366+giaramayo@users.noreply.github.com>
2022-06-28 08:37:25 -0300

    Se agrega spinner en la pantalla de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { LoginService } from '../../service/login.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -12,43 +12,49 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-public logo: string;
-public formLogin: FormGroup;
-private home: string = "/inicio"
+  public logo: string;
+  public formLogin: FormGroup;
+  private home: string = "/inicio"
+  public cargando: boolean;
 
-constructor(private loginService: LoginService,
-            public _snackBar: MatSnackBar,
-            private router: Router) { 
-  this.logo = 'assets/animacionLg.gif'
+  constructor(private loginService: LoginService,
+    public _snackBar: MatSnackBar,
+    private router: Router) {
+    this.logo = 'assets/animacionLg.gif';
+    this.cargando = false;
 
-  this.formLogin = new FormGroup({
-    usuario: new FormControl('', Validators.required),
-    contrasenia: new FormControl('', Validators.required)
-  });
+    this.formLogin = new FormGroup({
+      usuario: new FormControl('', Validators.required),
+      contrasenia: new FormControl('', Validators.required)
+    });
 
-}
+  }
 
   ingresar() {
+    this.cargando = true;
     let usuario = {
       email: this.formLogin.get('usuario')?.value,
       password: this.formLogin.get('contrasenia')?.value
     }
     this.loginService.ingresar(usuario)
-      .subscribe( resp => {
+      .subscribe(resp => {
         this.loginService.setToken(resp.data.token);
         this.router.navigateByUrl(this.home);
-      }, 
-      err => {
-        console.log(err)
-        this._snackBar.openFromComponent(DialogSnackbarComponent, {
-          data: { icono: 'report', mensaje: "No se encontró el usuario ingresado", titulo: 'Usuario Incorrecto' },
-          duration: 4000,
-          horizontalPosition: "right",
-          verticalPosition: "bottom",
-          panelClass: ["snack-bar-war"]
-        });
-      })
-
+      },
+        err => {
+          console.log(err)
+          this._snackBar.openFromComponent(DialogSnackbarComponent, {
+            data: { icono: 'report', mensaje: "No se encontró el usuario ingresado", titulo: 'Usuario Incorrecto' },
+            duration: 4000,
+            horizontalPosition: "right",
+            verticalPosition: "bottom",
+            panelClass: ["snack-bar-war"]
+          });
+          this.cargando = false;
+        },
+        () => {
+          this.cargando = false;
+        })
   }
 
 }
